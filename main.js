8ea810a8bb99429752891084a77f9f57f0cf87d5
Juan Manuel <179845632+JVillarez-m@users.noreply.github.com>
2025-01-05 20:06:24 -0500

    Add pawn movements & fix some issues

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
-const infoDispley = document.querySelector('#info-display')
+const infoDisplay = document.querySelector('#info-display')
 const width = 8
+
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -20,12 +23,12 @@ function createBoard() {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
-        square.setAttribute('squard-id', i)
+        square.firstChild && square.firstChild.setAttribute('draggable', true)
+        square.setAttribute('square-id', i)
         const row = Math.floor( (63 - i) / 8 ) + 1
         if (row % 2 === 0) {
             square.classList.add(i % 2 === 0 ? "beige" : "brown")
-        }
-        else {
+        } else {
             square.classList.add(i % 2 === 0 ? "brown" : "beige")
         }
 
@@ -34,7 +37,7 @@ function createBoard() {
         }
         
         if (i >= 48) {
-            square.firstChild.firstChild.classList.add('grey')
+            square.firstChild.firstChild.classList.add('white')
         }
 
         gameBoard.append(square)
@@ -42,3 +45,95 @@ function createBoard() {
 }
 
 createBoard()
+
+const allSquares = document.querySelectorAll('.square')
+
+allSquares.forEach((square) => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+    
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+    const taken = e.target.classList.contains('piece')
+    const valid = checkIfValid(e.target)
+    const opponentGo = playerGo === 'white' ? 'black' : 'white'
+    const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+
+    if (correctGo) {
+        if (takenByOpponent && valid) {
+            e.target.parentNode.append(draggedElement)
+            e.target.remove()
+            changePlayer()
+            return
+        }
+    }
+
+    if (taken && !takenByOpponent) {
+        infoDisplay.textContent = 'you cannot go here!'
+        setTimeout(() => infoDisplay.textContent = '', 2000);
+        return
+    }
+
+    if (valid) {
+        e.target.append(draggedElement)
+        changePlayer()
+    }
+
+    // e.target.append(draggedElement)
+}
+
+function changePlayer() {
+    if (playerGo === 'black') {
+        reverseIds()
+        playerGo = 'white'
+        playerDisplay.textContent = 'white'
+    } else {
+        revertIds()
+        playerGo = 'black'
+        playerDisplay.textContent = 'black'
+    }
+}
+
+function reverseIds() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) => {
+        square.setAttribute('square-id', (width * width - 1) - i )
+    })
+}
+
+function revertIds() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) => square.setAttribute('square-id', i))
+}
+
+function checkIfValid(target) {
+    const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+    const startId = Number(startPositionId)
+    const piece = draggedElement.id
+
+    switch (piece) {
+        case 'pawn':
+            const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+
+            if (starterRow.includes(startId) && startId + width * 2 === targetId || startId + width === targetId) {
+                return true
+            }
+    }
+}
